b8f02323f86856665b73ee5d8880b83ebe6f4c60
Ayush Raj <ayushrajpatna0011@gmail.com>
2024-03-16 23:23:14 +0530

    adiing sum.java

--- Sum.java
+++ Sum.java
@@ -5,6 +5,6 @@ class Sum
     public static void main(String args[]){
         int a=7;
         int b=8;
-        System.out.println(a+b);
+        System.out.println("The sum is "+ a+b);
     }
 }
